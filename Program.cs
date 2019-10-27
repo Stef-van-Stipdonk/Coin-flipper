3b313e284ffe98ea830c62a504268ab035fe336e
Vash <mrbats1234@gmail.com>
2019-10-27 21:41:52 +0100

    Added basic file and read line functionality

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Coin_flipper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number of your choice: ");
+            try
+            {
+                int intNumb = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("This is not a number");
+            }
+
+            Random r = new Random();
+
+            int ranNumb = r.Next(1, 2);
+
+            Console.WriteLine(ranNumb);
+        }
+    }
+}
