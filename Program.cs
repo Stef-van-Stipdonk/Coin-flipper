3a34e857764027f65d54aab493ccb817a44cd4fc
Stef van Stipdonk <mrbats1234@gmail.com>
2019-11-05 13:27:07 +0100

    Amount of coins can be entered shows highest streak and total heads tail

--- Program.cs
+++ Program.cs
@@ -6,19 +6,83 @@ namespace Coin_flipper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number of your choice: ");
+            // Amount of coins to flip - in string
+            string flipAmount;
+            // Amount of coins to flip - converted to int
+            int flipAmountInt;
+            // Random numer
+            var rng = new Random();
+            // Chances
+            string chances;
+            // Chances int
+            decimal chancesDec = 0;
+            // How many heads have been flipped
+            int heads = 0;
+            // How many tails have been flipped
+            int tails = 0;
 
-            string enterNumber = Console.ReadLine();
-            try
+            // Current streak
+            int streak = 0;
+            // Longest head streak
+            int headStreak = 0;
+            // Longest tail streak
+            int tailStreak = 0;
+            // streak is heads
+            int streakIsHeads = 0;
+
+            Console.WriteLine("How many coins do you want to flip?");
+            flipAmount = Console.ReadLine();
+            flipAmountInt = Convert.ToInt32(flipAmount);
+
+            // Console.WriteLine("What are the chances?");
+            // Console.WriteLine("If you enter 0.3 heads will have a 30% chance of winning and tails has 70%");
+            // Console.WriteLine("Enter a number:");
+            // chances = Console.ReadLine();
+            // chancesDec = decimal.Parse(chances);
+
+            for (int i = 0; i < flipAmountInt; i++)
             {
-                Convert.ToInt32(enterNumber);
-
+                if (rng.NextDouble() < 0.5)
+                {
+                    if (streakIsHeads != 0)
+                    {
+                        streak = 0;
+                    }
+                    streakIsHeads = 1;
+                    heads++;
+                    streak++;
+                    if (streak > headStreak)
+                    {
+                        headStreak = streak;
+                    }
+                }
+                else
+                {
+                    if (streakIsHeads != 1)
+                    {
+                        streak = 0;
+                    }
+                    streakIsHeads = 0;
+                    tails++;
+                    streak++;
+                    if (streak > tailStreak)
+                    {
+                        tailStreak = streak;
+                    }
+                }
             }
-            catch
+            Console.WriteLine("Heads has been flipped: " + heads);
+            Console.WriteLine("Tails has been flipped: " + tails);
+            if (tailStreak > headStreak)
             {
-                Console.WriteLine("Error while converting to int");
+                Console.WriteLine("The longest streak belongs to tails: " + tailStreak);
             }
-
+            else
+            {
+                Console.WriteLine("The longest streak belongs to heads: " + headStreak);
+            }
+            Console.WriteLine("\nPress enter to continue");
+            Console.ReadLine();
         }
     }
 }
